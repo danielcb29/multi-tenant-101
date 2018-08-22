32514a189458b37841377c242e849bfd5a4d76df
danielcb29 <danielc.web@gmail.com>
2018-08-21 23:05:34 -0500

    Printing tenant data

--- multicars/companies/views.py
+++ multicars/companies/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from cars.models import Car
 
 # Create your views here.
-class CompanyInformationView(TemplateView):
+class CompanyInformationView(ListView):
     template_name = 'companies/tenant.html'
+    model = Car
 
 class PublicCompanyView(TemplateView):
     template_name = 'companies/public.html'
